029135f5f3b22a0bf879921c1270adcb6d56258a
kayanhales <daybreakbliss@gmail.com>
2017-12-09 12:08:14 -0500

    renamed cartitem and removed comments

--- beauty-app-v1/src/CartItem.js
+++ beauty-app-v1/src/CartItem.js
@@ -10,25 +10,14 @@ class CartItem extends Component {
   }
   render() {
     return (
-      /*<div className="row">*/
         <div className="col-sm-6 col-md-4">
           <div className="thumbnail">
-            {/*<img src="..." alt="...">*/}
             <div className="caption">
               <h3>{this.props.itemTitle}</h3>
               <h4>Cost: {this.props.price}</h4>
-              {/*<p>
-                <a href="#" class="btn btn-primary" role="button">
-                  Button
-                </a>{" "}
-                <a href="#" class="btn btn-default" role="button">
-                  Button
-                </a>
-              </p>*/}
             </div>
           </div>
         </div>
-      /*</div>*/
     );
   }
 }
